c7883e2fa1ad584c1dd5fef9e77d915f01fe181c
이영욱 <lyuk98@hotmail.com>
2024-01-27 20:57:48 +0900

    Fix: export 오류 수정
    Layout.jsx에서 존재하지 않는 function을 export하려는 문제를 해결합니다

--- front/src/pages/Layout.jsx
+++ front/src/pages/Layout.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 
-function App() {
+function Layout() {
 	return (
 		<>
 			<header className="fixed bottom-0 left-0 right-0 px-8 py-2 bg-[#efefef]">
