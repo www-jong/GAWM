33d36cc8ac55d159d5fa1af9d6274fd3b7f66657
Bada <diduedidue@naver.com>
2024-01-29 17:50:31 +0900

    Feat:내감어때 페이지 기본구조 생성

--- front/src/pages/Browse/index.jsx
+++ front/src/pages/Browse/index.jsx
@@ -1,7 +1,21 @@
-function Browse() {
+import React from 'react';
+
+import Header from './components/Header';
+import Weather from './components/Weather';
+import OutfitSuggestion from './components/OutfitSuggestion';
+import TrendingSection from './components/TrendingSection';
+import FeedSection from './components/FeedSection';
+import Navigation from './components/Navigation';
+
+export default function Browse() {
 	return (
-		<p className="font-pretendard">내감어때</p>
+		<div className="App">
+			<Header />
+			<Weather />
+			<OutfitSuggestion />
+			<TrendingSection />
+			<FeedSection />
+			<Navigation />
+		</div>
 	);
 }
-
-export default Browse;
