334951df6f17fcce6f3c60f7346dd5bf8369a28e
이영욱 <lyuk98@hotmail.com>
2024-01-31 05:24:26 +0900

    Test: LiveScrollPane 테스트 코드

--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -12,6 +12,70 @@ function Home() {
 				<LiveScrollPane
 					components={
 						[
+							(
+								<LiveThumbnail
+									image="https://upload.wikimedia.org/wikipedia/commons/1/19/Giant_Panda.jpg"
+									title="판다 테스트"
+									createdDate={new Date(2024, 0, 30, 17, 45)}
+									points={1}
+								/>
+							),
+							(
+								<LiveThumbnail
+									image="https://upload.wikimedia.org/wikipedia/commons/1/19/Giant_Panda.jpg"
+									title="판다 테스트 2"
+									createdDate={new Date(2024, 0, 30, 17, 45)}
+									points={2}
+								/>
+							),
+							(
+								<LiveThumbnail
+									image="https://upload.wikimedia.org/wikipedia/commons/1/19/Giant_Panda.jpg"
+									title="판다 테스트 3"
+									createdDate={new Date(2024, 0, 30, 17, 45)}
+									points={3}
+								/>
+							),
+							(
+								<LiveThumbnail
+									image="https://upload.wikimedia.org/wikipedia/commons/1/19/Giant_Panda.jpg"
+									title="판다 테스트 4"
+									createdDate={new Date(2024, 0, 30, 17, 45)}
+									points={4}
+								/>
+							),
+							(
+								<LiveThumbnail
+									image="https://upload.wikimedia.org/wikipedia/commons/1/19/Giant_Panda.jpg"
+									title="판다 테스트 5"
+									createdDate={new Date(2024, 0, 30, 17, 45)}
+									points={5}
+								/>
+							),
+							(
+								<LiveThumbnail
+									image="https://upload.wikimedia.org/wikipedia/commons/1/19/Giant_Panda.jpg"
+									title="판다 테스트 6"
+									createdDate={new Date(2024, 0, 30, 17, 45)}
+									points={6}
+								/>
+							),
+							(
+								<LiveThumbnail
+									image="https://upload.wikimedia.org/wikipedia/commons/1/19/Giant_Panda.jpg"
+									title="판다 테스트 7"
+									createdDate={new Date(2024, 0, 30, 17, 45)}
+									points={7}
+								/>
+							),
+							(
+								<LiveThumbnail
+									image="https://upload.wikimedia.org/wikipedia/commons/1/19/Giant_Panda.jpg"
+									title="판다 테스트 8"
+									createdDate={new Date(2024, 0, 30, 17, 45)}
+									points={8}
+								/>
+							)
 						]
 					}
 				/>
