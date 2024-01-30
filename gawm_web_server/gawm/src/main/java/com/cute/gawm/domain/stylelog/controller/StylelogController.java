4e2ca2291ec380d5075f70ffb0caa225bdfb2aa3
www-jong <weon1009@gmail.com>
2024-01-31 03:22:36 +0900

    Feat: stylelog 조회기능 추가

--- gawm_web_server/gawm/src/main/java/com/cute/gawm/domain/stylelog/controller/StylelogController.java
+++ gawm_web_server/gawm/src/main/java/com/cute/gawm/domain/stylelog/controller/StylelogController.java
@@ -1,10 +1,19 @@
 package com.cute.gawm.domain.stylelog.controller;
 
 
+import com.cute.gawm.common.response.ErrorResponse;
+import com.cute.gawm.common.response.PagingResponse;
+import com.cute.gawm.domain.stylelog.entity.Stylelog;
 import com.cute.gawm.domain.stylelog.service.StylelogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/stylelog")
@@ -13,5 +22,26 @@ public class StylelogController {
     @Autowired
     private StylelogService stylelogService;
 
+    @GetMapping("/list")
+    public ResponseEntity<?> getStylelogsByMonth(@RequestParam("year") int year, @RequestParam("month") int month) {
+        try {
+            List<Stylelog> stylelogs = stylelogService.getStylelogsByMonth(year, month);
+            PagingResponse pagingResponse = new PagingResponse(
+                    HttpStatus.OK.value(),
+                    stylelogs,
+                    true, // isFirst
+                    true, // isLast
+                    0, // page
+                    1, // totalPage
+                    stylelogs.size(), // size
+                    false, // sorted
+                    false, // asc
+                    false // filtered
+            );
 
+            return ResponseEntity.ok(pagingResponse);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),"NotFoundException","데이터 처리 실패" + e.getMessage()));
+        }
+    }
 }
---
+++ gawm_web_server/gawm/src/main/java/com/cute/gawm/domain/stylelog/repository/StylelogRepository.java
@@ -0,0 +1,13 @@
+package com.cute.gawm.domain.stylelog.repository;
+
+import com.cute.gawm.domain.stylelog.entity.Stylelog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface StylelogRepository extends JpaRepository<Stylelog, Long> {
+
+    List<Stylelog> findByCreatedAtBetween(Timestamp startDate, Timestamp endDate);
+}
