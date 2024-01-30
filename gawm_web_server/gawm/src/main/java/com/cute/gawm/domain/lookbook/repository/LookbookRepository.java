e486a010ed1eb02234e4c8ceb865881c4aaba0f6
배서은 <baeseoeun1221@gmail.com>
2024-01-30 15:51:22 +0900

    Feat: querydsl config 추가

--- gawm_web_server/gawm/src/main/java/com/cute/gawm/domain/lookbook/repository/LookbookRepository.java
+++ gawm_web_server/gawm/src/main/java/com/cute/gawm/domain/lookbook/repository/LookbookRepository.java
@@ -1,8 +1,10 @@
 package com.cute.gawm.domain.lookbook.repository;
 
+import com.cute.gawm.domain.lookbook.entity.Lookbook;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LookbookRepository {
+public interface LookbookRepository extends JpaRepository<Lookbook, Integer>, LookbookRepositoryCustom {
 
 }
--- gawm_web_server/gawm/src/main/java/com/cute/gawm/domain/lookbook/repository/LookbookRepositoryCustomImpl.java
+++ gawm_web_server/gawm/src/main/java/com/cute/gawm/domain/lookbook/repository/LookbookRepositoryCustomImpl.java
@@ -1,4 +1,13 @@
 package com.cute.gawm.domain.lookbook.repository;
 
-public class LookbookRepositoryCustomImpl {
+import com.cute.gawm.common.QueryDslSupport;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+public class LookbookRepositoryCustomImpl extends QueryDslSupport implements LookbookRepositoryCustom{
+    public LookbookRepositoryCustomImpl(Class<?> clazz, EntityManager entityManager) {
+        super(clazz, entityManager);
+    }
 }
