8a5bcb798908ec753819081de793a357273a3abc
이영욱 <lyuk98@hotmail.com>
2024-01-31 05:24:00 +0900

    Feat: LiveScrollPane 추가
    가로로 scroll할 수 있는 LiveThumbnail의 container를 추가합니다

--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -1,10 +1,21 @@
 import TopBar from "../../components/TopBar";
+import LiveScrollPane from "./LiveScrollPane";
 import Title from "./Title";
+import LiveThumbnail from "../../components/LiveThumbnail";
 
 function Home() {
 	return (
 		<>
 			<TopBar title={<Title />} />
+
+			<div className="flex flex-col gap-2 pt-4">
+				<LiveScrollPane
+					components={
+						[
+						]
+					}
+				/>
+			</div>
 		</>
 	);
 }
