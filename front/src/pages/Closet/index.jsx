d0620f9c9e69a6a32b07d27d33e590a1fd807ce3
이영욱 <lyuk98@hotmail.com>
2024-01-31 17:44:22 +0900

    Feat: 내 옷장 페이지 초안 작성

--- front/src/pages/Closet/index.jsx
+++ front/src/pages/Closet/index.jsx
@@ -1,7 +1,18 @@
-function Closet() {
+import TopBar from "../../components/TopBar";
+
+export default function Closet() {
 	return (
-		<p className="font-pretendard">내 옷장</p>
+		<>
+			<div className="fixed top-0 left-0 right-0 mx-2.5 my-1.5">
+				<TopBar title={<h1 className="font-bold text-2xl">내 옷장</h1>} />
+			</div>
+
+			<div className="flex flex-col gap-4 mt-12 px-2.5">
+				<div className="flex flex-row gap-2">
+					<button className="basis-24 p-2 bg-[#d9d9d9] rounded-lg">옷 추가</button>
+					<button className="basis-24 p-2 bg-[#d9d9d9] rounded-lg">감각 추가</button>
+				</div>
+			</div>
+		</>
 	);
 }
-
-export default Closet;
