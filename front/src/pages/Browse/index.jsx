1293e0bcd44bfa319b9ac397cafe1dee0df0911c
서지수 <ssafybada@gmail.com>
2024-02-01 16:55:29 +0900

    Refactor: '내감어때' 컴포넌트화 리팩토링

--- front/src/pages/Browse/index.jsx
+++ front/src/pages/Browse/index.jsx
@@ -2,20 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import logoImage from '../../assets/images/HomeLogo.svg';
 import LiveImg from './LiveImg.png';
-
-const TodayLookComponent = ({ lookImage, userId, profileImage }) => {
-    return (
-        <div className="relative rounded-lg shadow-md w-full overflow-hidden mt-1">
-            <img className="w-full h-60 object-cover" src={lookImage} alt="Lookbook" />
-            <div className="absolute bottom-0 left-0 p-4 w-full flex justify-between items-end">
-                <div className="flex items-center">
-                    <img className="w-10 h-10 object-cover rounded-full border-2 border-white" src={profileImage} alt={userId} />
-                    <span className="ml-2 text-sm font-semibold text-white">{userId}</span>
-                </div>
-            </div>
-        </div>
-    );
-};
+import TodayLookComponent from './TodayLookComponent.jsx';
+import LiveComponent from './LiveComponent.jsx';
 
 
 export default function Browse() {
@@ -38,7 +26,7 @@ export default function Browse() {
     const Header = () => {
         return (
             <div className="fixed mt-1.5 ml-2.5">
-                <img src={logoImage} alt="Logo" className="w-auto" />
+                <img src={logoImage} alt="Logo" className="w-auto display: block" />
             </div>
         );
     }
@@ -65,7 +53,10 @@ export default function Browse() {
     const LiveSection = ({ title }) => {
         return (
             <div className="live-section mt-4">
-                <h2 className="h2-nps">{title}</h2>
+                <div className="flex">
+                <h2 className="h2-nps inline-block">{title}</h2>
+                <img src={LiveImg} alt="Live" className="w-8 h-5 inline-block" />
+                </div>
 
                 <div className="flex gap-2 mt-1 justify-center">
                     <LiveComponent />
@@ -81,45 +72,6 @@ export default function Browse() {
             </div>
         );
     };
-
-    function getRelativeTime(from) {
-        if (!(from instanceof Date) || isNaN(from)) {
-            return 'Invalid date';
-        }
-
-
-        const units = {
-            "day": 24 * 60 * 60 * 1000,
-            "hour": 60 * 60 * 1000,
-            "minute": 60 * 1000,
-            "second": 1000
-        }
-    
-        const formatter = new Intl.RelativeTimeFormat(
-            "ko",
-            { style: "short" }
-        );
-        const elapsed = from - new Date();
-    
-        for(const [unit, value] of Object.entries(units)) {
-            if(Math.abs(elapsed) > value || unit === "second")
-                return formatter.format(Math.round(elapsed / value), unit);
-        }
-    }
-
-    const LiveComponent = ({ image, title, createdDate, points }) => {
-        return (
-            <div className="w-26 h-26 rounded-lg relative">
-                <img className="w-full h-full object-cover rounded-lg" src={image} />
-                <div className="absolute bottom-0 left-0 right-0 h-9 bg-black opacity-70 rounded-b-lg leading-[0.5rem] px-0.5">
-                    <span className="inline-block text-sm text-white">{title}</span>
-                    <span className="inline-block text-[0.6rem] text-tertiary">
-                        {getRelativeTime(createdDate)} · {points} 감 포인트
-                    </span>
-                </div>
-            </div>
-            );
-        };
     
 
     return (
