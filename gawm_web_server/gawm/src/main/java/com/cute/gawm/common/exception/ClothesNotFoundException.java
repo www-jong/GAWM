5e501e43b938997d091617afccf1d933d0e70904
원종현 <weon1009@gmail.com>
2024-02-02 15:46:28 +0900

    Add: Custom Exception 추가

---
+++ gawm_web_server/gawm/src/main/java/com/cute/gawm/common/exception/ClothesNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cute.gawm.common.exception;
+
+public class ClothesNotFoundException extends RuntimeException {
+    public ClothesNotFoundException(String message) {
+        super(message);
+    }
+}
