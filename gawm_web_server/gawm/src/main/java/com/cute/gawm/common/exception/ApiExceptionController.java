970a1cea8af3fdcd2295819180ece6808f2a9340
주혜련 <object1997@naver.com>
2024-02-02 22:27:04 +0900

    Fix: 코드리뷰 후, 수정사항 반영

--- gawm_web_server/gawm/src/main/java/com/cute/gawm/common/exception/ApiExceptionController.java
+++ gawm_web_server/gawm/src/main/java/com/cute/gawm/common/exception/ApiExceptionController.java
@@ -26,83 +26,83 @@ public class ApiExceptionController {
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED) //401
     public ErrorResponse illegalExHandle(IllegalArgumentException e) {
-        return logAndResponse(e,HttpStatus.UNAUTHORIZED,"IllegalArgumentException");
+        return constructErrorResponse(e,HttpStatus.UNAUTHORIZED,"IllegalArgumentException");
     }
 
     // 부적절한 객체상태오류
     @ExceptionHandler(IllegalStateException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED) //401
     public ErrorResponse illegalExHandle(IllegalStateException e) {
-        return logAndResponse(e,HttpStatus.UNAUTHORIZED, "IllegalStateException");
+        return constructErrorResponse(e,HttpStatus.UNAUTHORIZED, "IllegalStateException");
     }
 
     // 잘못된 요청 데이터
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationExceptions(MethodArgumentNotValidException e) {
-        return logAndResponse(e,HttpStatus.BAD_REQUEST, "handleValidationExceptions");
+        return constructErrorResponse(e,HttpStatus.BAD_REQUEST, "handleValidationExceptions");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ResponseStatusException.class)
     public ErrorResponse responseStatusException(ResponseStatusException e) {
-        return logAndResponse(e,HttpStatus.BAD_REQUEST, "ResponseStatusException");
+        return constructErrorResponse(e,HttpStatus.BAD_REQUEST, "ResponseStatusException");
     }
 
     // 리소스를 찾을 수 없음
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNoHandlerFoundException(NoHandlerFoundException e) {
-        return logAndResponse(e,HttpStatus.NOT_FOUND, "handleNoHandlerFoundException");
+        return constructErrorResponse(e,HttpStatus.NOT_FOUND, "handleNoHandlerFoundException");
     }
 
     // 권한없음(접근거부)
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handleAccessDeniedException(AccessDeniedException e) {
-        return logAndResponse(e,HttpStatus.FORBIDDEN, "handleAccessDeniedException");
+        return constructErrorResponse(e,HttpStatus.FORBIDDEN, "handleAccessDeniedException");
     }
 
     // 권한없음(접근거부)
     @ExceptionHandler(AuthenticationException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ErrorResponse handleAuthenticationException(AuthenticationException e) {
-        return logAndResponse(e,HttpStatus.UNAUTHORIZED, "handleAuthenticationException");
+        return constructErrorResponse(e,HttpStatus.UNAUTHORIZED, "handleAuthenticationException");
     }
 
     // 파일업로드 에러
     @ExceptionHandler(S3FileUploadException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleFileUploadException(S3FileUploadException e) {
-        return logAndResponse(e,HttpStatus.INTERNAL_SERVER_ERROR, "S3FileUploadException");
+        return constructErrorResponse(e,HttpStatus.INTERNAL_SERVER_ERROR, "S3FileUploadException");
     }
 
     // 파일삭제에러
     @ExceptionHandler(S3FileDeleteException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleFileDeletionException(S3FileDeleteException e) {
-        return logAndResponse(e,HttpStatus.INTERNAL_SERVER_ERROR, "S3FileDeleteException");
+        return constructErrorResponse(e,HttpStatus.INTERNAL_SERVER_ERROR, "S3FileDeleteException");
     }
 
     // 실행중 에러
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) //500
     public ErrorResponse handleRuntimeException(RuntimeException e) {
-        return logAndResponse(e,HttpStatus.INTERNAL_SERVER_ERROR, "RuntimeException");
+        return constructErrorResponse(e,HttpStatus.INTERNAL_SERVER_ERROR, "RuntimeException");
     }
 
     // 유저를 찾을 수 없음.
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleUserNotFoundException(UserNotFoundException ex) {
-        return logAndResponse(ex, HttpStatus.NOT_FOUND, "UserNotFoundException");
+        return constructErrorResponse(ex, HttpStatus.NOT_FOUND, "UserNotFoundException");
     }
 
     // 옷을 찾을 수 없음
     @ExceptionHandler(ClothesNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleClothesNotFoundException(ClothesNotFoundException ex) {
-        return logAndResponse(ex, HttpStatus.NOT_FOUND, "ClothesNotFoundException");
+        return constructErrorResponse(ex, HttpStatus.NOT_FOUND, "ClothesNotFoundException");
     }
 
     // 데이터 request 에러
@@ -116,7 +116,7 @@ public class ApiExceptionController {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleAllUncaughtException(Exception e) {
-        return logAndResponse(e,HttpStatus.INTERNAL_SERVER_ERROR, "handleAllUncaughtException");
+        return constructErrorResponse(e,HttpStatus.INTERNAL_SERVER_ERROR, "handleAllUncaughtException");
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED) //401
@@ -129,10 +129,10 @@ public class ApiExceptionController {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) //500
     @ExceptionHandler(DataMismatchException.class)
     public ErrorResponse datamismatchExHandle(DataMismatchException e) {
-        return logAndResponse(e,HttpStatus.INTERNAL_SERVER_ERROR, "DataMismatchException");
+        return constructErrorResponse(e,HttpStatus.INTERNAL_SERVER_ERROR, "DataMismatchException");
     }
 
-    private ErrorResponse logAndResponse(Exception e, HttpStatus status, String errorType) {
+    private ErrorResponse constructErrorResponse(Exception e, HttpStatus status, String errorType) {
         log.error("[exceptionHandle] ex={}", e.getMessage(), e);
         return new ErrorResponse(status.value(), errorType, e.getMessage());
     }
