c2753e2f95789f18868c6bb25f4e8371854e8bc2
Bada <diduedidue@naver.com>
2024-02-05 08:30:40 +0900

    Feat: 구글 로그인 생성

---
+++ front/src/pages/Landing/SocialGoogle.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import google_login from '../../assets/images/google_login.svg';
+
+const SocialKakao = () => {
+    const KAKAO_CLIENT_ID = 'REST API KEY'; // REST API KEY = KAKAO_CLIENT_ID
+    const redirect_uri = 'http://localhost:4000/gawm-front/landing'; // Redirect URI
+    // oauth 요청 URL
+    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${redirect_uri}&response_type=code`;
+
+    const handleLogin = () => {
+        window.location.href = kakaoURL;
+    };
+
+    return (
+        <>
+            <div className="flex justify-center items-center">
+                <img src={google_login} alt="카카오 로그인" onClick={handleLogin} className="cursor-pointer" />
+            </div>
+        </>
+    );
+};
+
+export default SocialKakao;
--- front/src/pages/Landing/index.jsx
+++ front/src/pages/Landing/index.jsx
@@ -1,14 +1,17 @@
 import SocialKakao from './SocialKakao.jsx';
+import SocialGoogle from './SocialGoogle.jsx';
+import landImage1 from '../../assets/images/LandingPage_1.svg';
 
 
 export default function Landing() {
     return (
         <div className="h-screen">
             <div style={{ height: '70vh' }} >
-
+                <img src={landImage1} alt="랜딩이미지1" className="flex justify-center items-center" />
             </div>
-            <div style={{ height: '30vh' }} >
+            <div className="flex flex-col justify-center items-center gap-4" style={{ height: '30vh' }} >
                 <SocialKakao />
+                <SocialGoogle />
             </div>
         </div>
     );
