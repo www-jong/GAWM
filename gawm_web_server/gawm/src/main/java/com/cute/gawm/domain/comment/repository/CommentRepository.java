516260628886d2a50c3b85112264823082b919ae
주혜련 <object1997@naver.com>
2024-02-04 11:42:46 +0900

    Fix: Merge 충돌 해결

--- gawm_web_server/gawm/src/main/java/com/cute/gawm/domain/comment/repository/CommentRepository.java
+++ gawm_web_server/gawm/src/main/java/com/cute/gawm/domain/comment/repository/CommentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Integer> {
-    void deleteByStylelogId(int stylelogId);
+//    void deleteByStylelogId(int stylelogId);
 }
