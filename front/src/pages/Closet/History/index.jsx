6f323e67fa18798a9e0e9094c5a3f8bae8382702
서지수 <ssafybada@gmail.com>
2024-02-05 16:07:52 +0900

    Feat: '나의감각들' 내부 네비게이션 버튼 및 컴포넌트 렌더 설정

--- front/src/pages/Closet/History/index.jsx
+++ front/src/pages/Closet/History/index.jsx
@@ -1,5 +1,19 @@
+import React, { useState } from 'react';
+import Calendar from "./CalendarComponent.jsx";
+import MyLook from "./MyLookComponent.jsx";
+
 export default function History() {
+	const [selectedComponent, setSelectedComponent] = useState(null);
+
 	return (
-		<></>
+		<>
+			<div>
+				<button onClick={() => setSelectedComponent('calendar')}>캘린더</button>
+				<button onClick={() => setSelectedComponent('myLook')}>나의 감각들</button>
+			</div>
+
+			{selectedComponent === 'calendar' && <Calendar />}
+			{selectedComponent === 'myLook' && <MyLook />}
+		</>
 	)
 }
