a5afa85e83fed9b8763ced214d954e471c63d0eb
이영욱 <lyuk98@hotmail.com>
2024-02-06 17:33:45 +0900

    Feat: ListGroup 속성 추가
    div 속성을 추가합니다

--- front/src/components/ListGroup/index.jsx
+++ front/src/components/ListGroup/index.jsx
@@ -4,16 +4,15 @@
  * - className: 내부 <ul> 태그에 적용할 class
  * - onClick: 클릭 시 실행할 함수
  * - children: 컨테이너 내부의 요소
+ * - div: 속성 존재 시 <div> 생성
  * 
  * @returns 생성된 JSX component
  */
 export default function ListGroup({ className, onClick, children }) {
-	return (
-		<ul
-			className={`list-none grid grid-cols-1 divide-y ${className ? className : ""}`}
-			onClick={onClick}
-		>
-			{children}
-		</ul>
-	);
+	const appliedClassName = `list-none grid grid-cols-1 divide-y ${className ? className : ""}`;
+
+	if("div" in arguments[0])
+		return <div className={appliedClassName} onClick={onClick}>{children}</div>;
+	else
+		return <ul className={appliedClassName} onClick={onClick}>{children}</ul>;
 }
