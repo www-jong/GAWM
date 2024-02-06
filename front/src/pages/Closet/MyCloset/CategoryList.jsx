e0122744ca7ece5a3898b58d930b6577b7073c9e
이영욱 <lyuk98@hotmail.com>
2024-02-06 17:48:34 +0900

    Feat: 옷 세부 정보 UI 생성 및 기존 UI 수정
    테스트 정보를 통해 옷의 세부 정보를 표시합니다

--- front/src/pages/Closet/MyCloset/CategoryList.jsx
+++ front/src/pages/Closet/MyCloset/CategoryList.jsx
@@ -1,6 +1,15 @@
 import ListGroup from "../../../components/ListGroup";
 import ListItem from "../../../components/ListGroup/ListItem";
 
+/**
+ * ListGroup을 확장하여 옷의 카테고리를 포함하는 component를 생성합니다
+ * 
+ * - className: 생성될 ListGroup의 className 속성
+ * - categories: 아이템으로 표시할 카테고리 배열
+ * - categorySetter: 카테고리 클릭 시 호출할 함수
+ * 
+ * @returns 생성된 JSX component
+ */
 export default function CategoryList({ className, categories, categorySetter }) {
 	return (
 		<ListGroup className={className ? className : ""}>
@@ -8,6 +17,7 @@ export default function CategoryList({ className, categories, categorySetter }) 
 				categories.map(
 					(category) => (
 						<ListItem
+							className="py-4"
 							key={category}
 							onClick={() => {categorySetter(category)}}
 						>
