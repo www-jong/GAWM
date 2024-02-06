606bea5030a5ce8534dcc2de97a116db6fed9b78
seoeunbae <enyoupark@gmail.com>
2024-02-07 03:04:57 +0900

    Feat: 커밋을 위한 커밋..

--- front/dev-dist/sw.js
+++ front/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4mmb8akijfg"
+    "revision": "0.lpnk4cc1o7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
