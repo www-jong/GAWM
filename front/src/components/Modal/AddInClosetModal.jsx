265793af84cd126f2df48c24719cceeddbe63147
서지수 <ssafybada@gmail.com>
2024-02-06 11:10:36 +0900

    Feat: 옷, 감각 추가 모달 click event 설정

--- front/src/components/Modal/AddInClosetModal.jsx
+++ front/src/components/Modal/AddInClosetModal.jsx
@@ -1,7 +1,15 @@
-export default function AddInCloset() {
-    return (
-      <>
-        <p>모달모달임하이요</p>
-      </>
-    );
-  }
+export default function AddInCloset({ onClose }) {
+  // 모달 자체 클릭은 안 닫히게 하려고(상위 emit? 막기)
+  const handleModalContentClick = (e) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-500" onClick={onClose}>
+      <div className="fixed bottom-0 right-0 mb-36 mr-3 p-4 bg-white rounded-lg shadow-lg" onClick={handleModalContentClick}>
+        <p>모달모달임하이하이요</p>
+        <button onClick={onClose}>닫기</button>
+      </div>
+    </div>
+  );
+}
