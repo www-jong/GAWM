6f61422e83431793abb04bfcb407b93112cc8bbe
이영욱 <lyuk98@hotmail.com>
2024-02-07 12:44:06 +0900

    Feat: 마이페이지 UI 작성
    마이페이지의 기본 UI를 작성합니다

--- front/src/pages/MyPage/index.jsx
+++ front/src/pages/MyPage/index.jsx
@@ -1,7 +1,38 @@
-function MyPage() {
+import { Outlet } from "react-router-dom";
+import TopBar from "../../components/TopBar";
+import ProfileBadge from "./ProfileBadge";
+import ListGroup from "../../components/ListGroup";
+import ListItem from "../../components/ListGroup/ListItem";
+import { BookmarkIcon, Cog6ToothIcon } from "@heroicons/react/24/outline";
+
+/**
+ * 마이페이지 페이지 영역을 생성합니다
+ * 
+ * @returns 생성된 JSX component
+ */
+export default function MyPage() {
 	return (
-		<p className="font-pretendard">마이페이지</p>
+		<>
+			<div className="fixed top-0 left-0 right-0 px-2.5 py-1.5 bg-white">
+				<TopBar title={<h1 className="font-bold text-2xl">마이페이지</h1>} />
+			</div>
+
+			<div className="flex flex-col gap-4 mt-12 mb-24 mx-auto px-2.5 md:px-0 w-full md:w-8/12 lg:w-6/12">
+				<ProfileBadge />
+
+				<ListGroup div>
+					<ListItem link href="/mypage/settings" className="flex flex-row items-center gap-2">
+						<Cog6ToothIcon className="size-6" />
+						<span>계정 설정</span>
+					</ListItem>
+					<ListItem link href="/mypage/bookmark" className="flex flex-row items-center gap-2">
+						<BookmarkIcon className="size-6" />
+						<span>북마크한 감각</span>
+					</ListItem>
+				</ListGroup>
+			</div>
+
+			<Outlet />
+		</>
 	);
 }
-
-export default MyPage;
