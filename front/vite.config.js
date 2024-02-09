29dc016d1a4d0f819aa07024f14649704e7f0bdd
서지수 <diduedidue@naver.com>
2024-02-09 18:11:01 +0900

    Config: 'vite.config.js'에 '@/src' 절대경로 alias 설정 추가
    Former-commit-id: e497b345b06c1587a6b6defbd3859d5246f51052

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path';
+
 
 export default defineConfig({
 	base: '/gawm/',
@@ -13,6 +15,11 @@ export default defineConfig({
 	server: {
 		port: 4000
 	},
+	resolve: {
+		alias: {
+		  '@': path.resolve(__dirname, './src')
+		}
+	  }
 	// build: {
 	// 	rollupOptions: {
 	// 		input: {
