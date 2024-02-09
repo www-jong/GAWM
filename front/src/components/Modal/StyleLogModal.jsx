376aeb51ed8a6af2aebe2794af1e53df3e48369e
서지수 <diduedidue@naver.com>
2024-02-09 18:27:34 +0900

    Feat: stylelog 모달 내에서 감각 추가 버튼 누를 시 navigate 추가
    Former-commit-id: 7291d353d6b47bb0982cb8c92634dccb2ca6e723

--- front/src/components/Modal/StyleLogModal.jsx
+++ front/src/components/Modal/StyleLogModal.jsx
@@ -3,19 +3,25 @@ import { useNavigate } from 'react-router-dom';
 import StyleLogPlus from '../../assets/images/StyleLogPlus.svg'
 
 export default function StyleLog({ date, onClose }) {
-  const navigate = useNavigate(); // 이거 왜써놨더라
+  const navigate = useNavigate();
   const modalRef = useRef();
 
   const handleModalContentClick = (e) => {
     e.stopPropagation();
   };
 
-  useEffect(() => {  // 모달 참조가 있을 경우 애니메이션 적용
+  useEffect(() => {  // 모달 참조가 있을 경우 애니메이션
     if (modalRef.current) {
       modalRef.current.classList.remove('translate-y-full');
       modalRef.current.classList.add('translate-y-0');
     }
   }, []);
+
+  const handleAddLook = () => {
+    navigate('/look/add');
+    onClose();
+  };
+
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end z-50" onClick={onClose}>
@@ -24,7 +30,7 @@ export default function StyleLog({ date, onClose }) {
         <div className="flex justify-start w-full mt-2">
           <p className="text-xl font-bold text-main">{date}의 감각</p>
         </div>
-        <img className="mt-6 w-full" src={StyleLogPlus} alt="스타일로그 추가 이미지" />
+        <img className="mt-4 mb-2 w-full" src={StyleLogPlus} onClick={handleAddLook} alt="스타일로그 추가 이미지" />
       </div>
     </div>
   );
