1132cef3cb83652981a66d979843433c482ba271
서지수 <diduedidue@naver.com>
2024-02-09 16:57:18 +0900

    Feat: 날짜 클릭시 StyleLogModal 기본 구조
    Former-commit-id: 48ee85eae101562b961627938c3b1afa6c43726e

--- front/src/components/Modal/StyleLogModal.jsx
+++ front/src/components/Modal/StyleLogModal.jsx
@@ -1,16 +1,28 @@
+import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export default function StyleLog({ onClose }) {
-  const navigate = useNavigate(); // useNavigate 훅
+export default function StyleLog({ date, onClose }) {
+  const navigate = useNavigate(); // 이거 왜써놨더라
+  const modalRef = useRef();
+
   const handleModalContentClick = (e) => {
     e.stopPropagation();
   };
 
+  useEffect(() => {  // 모달 참조가 있을 경우 애니메이션 적용
+    if (modalRef.current) {
+      modalRef.current.classList.remove('translate-y-full');
+      modalRef.current.classList.add('translate-y-0');
+    }
+  }, []);
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50" onClick={onClose}>
-      <div className="fixed bottom-0 right-0 mb-36 mr-3 p-4 bg-white rounded-xl shadow-lg flex flex-col items-center" onClick={handleModalContentClick}>
-        <p>옷뜨는모달모달임하이요</p>
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end z-50" onClick={onClose}>
+      <div ref={modalRef} className="fixed bottom-0 left-0 right-0 mx-auto p-4 bg-white rounded-t-xl shadow-lg flex flex-col items-center transition duration-300 ease-out translate-y-full" onClick={handleModalContentClick}>
+        <div className="w-8 h-1 rounded-xl mb-3 bg-main"></div>
+        <div className="flex justify-start w-full">
+          <p className="text-xl font-bold text-main">{date}의 감각</p>
+        </div>
       </div>
     </div>
   );
