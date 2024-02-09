db251529a6247aa74e18527a388b57bff8a7aa65
서지수 <diduedidue@naver.com>
2024-02-09 17:13:17 +0900

    Feat: StyleLog 모달 기록된 감각 없을시 추가 이미지 등록
    Former-commit-id: 1a6fa2b58230db82dcfce31ef9bbfe1cc69b6373

--- front/src/components/Modal/StyleLogModal.jsx
+++ front/src/components/Modal/StyleLogModal.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import StyleLogPlus from '../../assets/images/StyleLogPlus.svg'
 
 export default function StyleLog({ date, onClose }) {
   const navigate = useNavigate(); // 이거 왜써놨더라
@@ -20,9 +21,10 @@ export default function StyleLog({ date, onClose }) {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end z-50" onClick={onClose}>
       <div ref={modalRef} className="fixed bottom-0 left-0 right-0 mx-auto p-4 bg-white rounded-t-xl shadow-lg flex flex-col items-center transition duration-300 ease-out translate-y-full" onClick={handleModalContentClick}>
         <div className="w-8 h-1 rounded-xl mb-3 bg-main"></div>
-        <div className="flex justify-start w-full">
+        <div className="flex justify-start w-full mt-2">
           <p className="text-xl font-bold text-main">{date}의 감각</p>
         </div>
+        <img className="mt-6 w-full" src={StyleLogPlus} alt="스타일로그 추가 이미지" />
       </div>
     </div>
   );
