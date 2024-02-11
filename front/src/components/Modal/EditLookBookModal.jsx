76c36b2e8a799d388dda8c3d18f8e741da8bf2b7
서지수 <diduedidue@naver.com>
2024-02-12 03:09:07 +0900

    Feat: 룩북 삭제 API 연결
    Former-commit-id: 93b990e760e0e4fd77733ec41599810eb9b7f497

--- front/src/components/Modal/EditLookBookModal.jsx
+++ front/src/components/Modal/EditLookBookModal.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { updateLookbook, deleteLookbook } from '@/apis/lookbook.js'; // API 함수의 정확한 경로를 지정해주세요.
 
 export default function EditLookBook({ lookbookId, onClose, }) {
     const navigate = useNavigate();
@@ -19,25 +20,34 @@ export default function EditLookBook({ lookbookId, onClose, }) {
     }, []);
 
 
-    const handleEdit = () => {
-        navigate('/look/add');
+    const handleEdit = async () => {
+        navigate('/look/add');  // edit page 만들어야지.. 보내야할 정보들이 너무 많네
         onClose();
     };
 
-    const handleDelete = () => {
-        navigate('/look/add');
-        onClose();
+    const handleDelete = async () => {
+        if (window.confirm('정말로 삭제하시겠습니까?')) {  // 아니 윈도우컨펌 써도되나
+            try {
+                await deleteLookbook(lookbookId);
+                alert('룩북이 삭제되었습니다.');
+                navigate('/browse'); // 삭제 후 이동할 경로 지정
+            } catch (error) {
+                console.error('룩북 삭제 중 오류 발생:', error);
+                alert('룩북 삭제에 실패했습니다.');
+            }
+            onClose();
+        }
     };
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end z-50" onClick={onClose}>
-            <div ref={modalRef} className="fixed bottom-0 left-0 right-0 mx-auto p-4 bg-white shadow-lg flex flex-col items-center transition duration-300 ease-out translate-y-full" onClick={handleModalContentClick}>
-                <div className="flex flex-col justify-start w-full mt-2">
-                    <p className="text-xl font-bold text-main">수정하기</p>
-                    <p className="text-xl font-bold text-main">삭제하기</p>
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex z-50" onClick={onClose}>
+            <div ref={modalRef} className="fixed bottom-0 left-0 right-0 p-4 bg-white shadow-lg flex flex-col transition duration-300 ease-out translate-y-full" onClick={handleModalContentClick}>
+                <div className="flex flex-col w-full my-2 gap-4 font-semibold">
+                    <button onClick={handleEdit} className="text-left text-base">수정하기</button>
+                    <button onClick={handleDelete} className="text-left text-base">삭제하기</button>
                 </div>
-                <hr />
-                <div>
+                <hr className="w-full border-t border-gray-300 my-1"/>
+                <div className="text-main text-center ">
                     <p>취소</p>
                 </div>
             </div>
