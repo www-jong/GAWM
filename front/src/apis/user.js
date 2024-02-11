b9e5a6dd7cbda6ae4fe1d21fa2134949802eaa46
서지수 <diduedidue@naver.com>
2024-02-11 23:14:16 +0900

    Feat: user관련 api에 팔로우 리스트 받아오는거 추가
    Former-commit-id: b2ea5139d8428821ee14dd03b2bbc9353246725b

--- front/src/apis/user.js
+++ front/src/apis/user.js
@@ -40,3 +40,23 @@ export function edit(data) {
 		data
 	);
 }
+
+
+/**
+ * 로그인한 사용자의 팔로잉 목록을 받아오는 객체를 생성합니다
+ * 
+ * @param {Object} options 요청 옵션 (page, size, sortBy, sortDirection)
+ * @returns Promise 객체
+ */
+export function getFollowingList({ page = 0, size = 10, sortBy = 'create_at', sortDirection = 'asc' } = {}) {
+	return axios.get(`${prefix}/following`, {
+	  params: {
+		page,
+		size,
+		sortBy,
+		sortDirection
+	  },
+	//   withCredentials: true 해야하나 모르겠네
+	});
+  }
+  
