15f0e8bc61bc131eb2ee9cfd830ab02276ef0a13
서지수 <diduedidue@naver.com>
2024-02-11 17:16:42 +0900

    Feat: 감각 페이지 스와이퍼 및 조회수
    Former-commit-id: 4f1c63a8ad795ce9e66dddf281e3d4b9901777c4

--- front/src/pages/Look/index.jsx
+++ front/src/pages/Look/index.jsx
@@ -2,30 +2,53 @@ import LookTest from "./LookTest.json"
 import Backbutton from '@/components/Button/BackButton.jsx';
 import React, { useState } from 'react';
 import Mascot from '@/assets/images/Mascot.svg';
-import LookTestImg from './LookTestImg2.png';
-import { CSSTransition } from 'react-transition-group';
-import './index.css';
+import LookTestImg1 from './LookTestImg1.png';
+import LookTestImg2 from './LookTestImg2.png';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import BackButtonImg from '@/assets/images/back_button_img.png';
+import views from '@/assets/images/views.png';
+
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+// import './index.css';
 
 
 
 export default function Look() {
+    const { lookbookId, userId, userNickname, userProfileImg, createdAt, clothes, lookbookImgs, comment, likeCnt, view, tag, liked, bookmarked, followed } = LookTest.data;
+
     const [commentVisible, setCommentVisible] = useState(false);
     const [repliesVisible, setRepliesVisible] = useState(false);
     const [commentText, setCommentText] = useState("");
+    const [likes, setLikes] = useState(likeCnt);
+    const [commentModalVisible, setCommentModalVisible] = useState(false);
+    const [comments, setComments] = useState(LookTest.data.comment); // 코멘트 데이터들을 상태로 관리
 
-    const { lookbookId, userId, userNickname, userProfileImg, createdAt, clothes, lookbookImgs, comment, likeCnt, view, tag, liked, bookmarked, followed } = LookTest.data;
 
     const handleSubmit = (e) => {
-        e.preventDefault(); // 폼 제출 기본 동작 방지
+        e.preventDefault();
         console.log(commentText); // 입력된 댓글 로그
         setCommentText(""); // 입력 필드 초기화
     };
 
+    const handleLike = () => {
+        setLikes(likes + 1); // 좋아요 수 증가
+    };
+
+    const toggleCommentModal = () => {
+        setCommentModalVisible(!commentModalVisible); // 댓글 입력 창 토글
+    };
+
+
+
 
     return (
         <div className="mx-auto">
-            <Backbutton />
+
             <div className="h-full relative ">
+                <Backbutton />
 
                 <div className="flex justify-between items-center px-3 py-4 mt-2">
                     <div className="relative mt-1 flex">
@@ -37,7 +60,7 @@ export default function Look() {
                                 {userNickname}
                             </p>
                             <p className="text-gray-400 text-xs">
-                                2024년 2월 11일
+                                {createdAt}
                             </p>
                         </div>
                         {/* <span className="text-xs mx-2">•</span>
@@ -52,8 +75,88 @@ export default function Look() {
 
 
                 {/* 룩 이미지 */}
-                <div className="relative w-full pt" style={{ paddingTop: "100%" }}>
-                    <img className="absolute top-0 left-0 w-full h-full object-cover" src={LookTestImg} alt="룩이미지" />
+                <div className="relative w-full">
+                    <Swiper
+                        pagination={{ clickable: true }}
+                        modules={[Pagination]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide>
+                            <img src={LookTestImg1} alt="룩 이미지1" className="w-full h-96 object-cover" />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={LookTestImg2} alt="룩 이미지2" className="w-full h-96 object-cover" />
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+
+
+                {/* 좋아요 북마크 코멘트 등 */}
+                <div className="mx-1">
+                    <div className="flex justify-between items-center p-2">
+                        <div className="flex space-x-2 items-center">
+                            <button onClick={handleLike} className="focus:outline-none Like">
+                                <svg className="w-8 h-8 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.6" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+                                </svg>
+                            </button>
+                            <button onClick={toggleCommentModal} className="focus:outline-none Comment">
+                                <svg className="w-8 h-8 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.6" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path>
+                                </svg>
+                            </button>
+                            <button className="focus:outline-none save">
+                                <svg className="w-7 h-7 text-gray-600 z-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.6" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="flex items-center space-x-1">
+                            <img src={views} alt="조회수" className="w-5 h-5" />
+                            <span className="text-sm text-gray-400">{view}</span>
+                        </div>
+                    </div>
+                    <div className="p-2 flex flex-col space-y-3">
+                        <div className="w-full">
+                            <p className="font-bold text-sm text-gray-700">{likes} likes</p>
+                        </div>
+                        <div className="w-full">
+                            <p className="font-normal text-xs text-gray-500">10 hours ago</p>
+                        </div>
+                    </div>
+
+
+                    {/* 코멘트 모달 */}
+                    {commentModalVisible && (
+                        <div className="overflow-y-scroll w-full absolute inset-0 bg-white transform transition duration-200 z-50" style={{ maxHeight: "90vh" }}>
+                            <div className="border-b" onClick={() => setCommentModalVisible(false)}>
+                                <button className="mb-2 flex items-start">
+                                    <img src={BackButtonImg} alt="뒤로 가기" className="size-6 ml-2 mt-1" />
+                                </button>
+                            </div>
+                            <div className="p-2 mb-10">
+
+                                {/* 코멘트 내용 */}
+                                {comments.map((comment) => (
+                                    <div key={comment.commentId} className="flex justify-start flex-col space-y-3 items-start px-2 border-b border-gray-100">
+                                        <div className="relative mt-1 mb-3 pt-2 flex">
+                                            <div className="mr-2">
+                                                <img src={comment.userProfileImg ? comment.userProfileImg : Mascot} alt={comment.userNickname} className="w-8 h-8 rounded-full object-cover bg-quaternary max-w-none" />
+                                            </div>
+                                            <div className="ml-2 w-full">
+                                                <p className="text-gray-600 md:text-sm text-xs w-full">
+                                                    <span className="text-gray-900 font-semibold">{comment.userNickname}</span> {comment.content}
+                                                </p>
+                                                <div className="time mt-1 text-gray-400 text-xs">
+                                                    <p>2d</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    )}
                 </div>
 
 
@@ -62,10 +165,10 @@ export default function Look() {
                     <form onSubmit={handleSubmit}>
                         <div className="flex justify-between border-t items-center w-full">
                             <div className="w-full">
-                                <input 
-                                    type="text" 
-                                    name="comment" 
-                                    id="comment" 
+                                <input
+                                    type="text"
+                                    name="comment"
+                                    id="comment"
                                     placeholder="댓글을 남겨보세요..."
                                     className="w-full text-sm py-4 px-3 rounded-none focus:outline-none"
                                     value={commentText}
@@ -80,7 +183,7 @@ export default function Look() {
                         </div>
                     </form>
                 </div>
-                
+
 
             </div>
         </div>
---
+++ front/src/pages/Look/test.tsx
@@ -0,0 +1,221 @@
+<!-- This is an example component -->
+<script defer src="https://unpkg.com/alpinejs@3.1.1/dist/cdn.min.js"></script>
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Varela&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Karla:wght@200&display=swap');
+
+	* {
+		font-family: 'Nunito', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		direction: ltr;
+		font-weight: 500;
+	}
+</style>
+
+
+<div class="mx-auto flex justify-center items-center filter blur-2xl animate-pulse duration-500 transition w-full">
+	<div class="mt-20 mr-10 flex relative">
+		<div class="p-44 rounded-full bg-gradient-to-r to-indigo-700 from-pink-900 absolute top-20 right-0">
+
+		</div>
+		<div class="p-44 rounded-full bg-gradient-to-r to-pink-700 from-indigo-900 absolute md:flex hidden">
+
+		</div>
+	</div>
+	<!-- Right Side -->
+	<div class="flex flex-col absolute top-8 right-10 space-y-4">
+
+		<div class="p-5 rounded-full bg-gradient-to-r to-pink-700 via-red-500 from-indigo-900 absolute right-16 top-10">
+
+		</div>
+	</div>
+	<div class="flex flex-col absolute bottom-8 right-10 space-y-4">
+
+		<div class="p-10 rounded-full bg-gradient-to-r to-pink-700 from-indigo-900 absolute right-16 bottom-10">
+
+		</div>
+	</div>
+	<!--  Left side -->
+	<div class="flex flex-col space-y-4 filter animate-pulse duration-500">
+		<div class="p-10 bg-gradient-to-r to-indigo-700 from-blue-900 absolute top-20 left-20">
+
+		</div>
+		<div class="p-10 bg-gradient-to-r to-indigo-700 from-blue-900 absolute bottom-20 right-20">
+
+		</div>
+	</div>
+</div>
+
+<div class="mx-auto flex justify-center max-w-3xl md:mb-8 mt-4 bg-white rounded-lg items-center relative md:p-0 p-8"
+	x-data="{
+        comment : false,
+    }">
+
+	<div class="h-full relative">
+		<div class="py-2 px-2">
+			<div class="flex justify-between items-center py-2">
+				<div class="relative mt-1 flex">
+					<div class="mr-2">
+						<img src="https://avatars.githubusercontent.com/u/68494287?v=4" alt="saman sayyar"
+                                class="w-10 h-10 rounded-full object-cover">
+                        </div>
+						<div class="ml-3 flex justify-start flex-col items-start">
+							<p class="text-gray-900 text-sm">
+								samansayyar
+							</p>
+							<p class="text-gray-600 text-xs">
+								samansayyar
+							</p>
+						</div>
+						<!-- <span class="text-xs mx-2">•</span>
+                       <button class="text-indigo-500 text-sm capitalize flex justify-start items-start">follow</button> -->
+					</div>
+					<button type="button"
+                        class="relative p-2 focus:outline-none border-none bg-gray-100 rounded-full">
+                        <svg class="w-5 h-5 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z">
+                            </path>
+                        </svg>
+                    </button>
+				</div>
+			</div>
+			<div class="relative w-full h-full">
+				<img src="https://wallpaperaccess.com/full/345330.jpg" alt="saman" class="rounded-lg w-full h-full object-cover">
+            </div>
+				<div class="">
+
+					<!-- Comment -->
+					<div class="overflow-y-scroll w-full absolute inset-0 bg-white transform transition duration-200"
+						x-show="comment" x-transition:enter="transition ease-out duration-200"
+						x-transition:enter-start="opacity-0 transform scale-90"
+						x-transition:enter-end="opacity-100 transform scale-100"
+						x-transition:leave="transition ease-in duration-100"
+						x-transition:leave-start="opacity-100 transform scale-100"
+						x-transition:leave-end="opacity-0 transform scale-90">
+						<div class="flex justify-start items-center py-2 px-4 border-b" @click="comment = !comment">
+							<svg class="w-8 h-8 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+									d="M7 16l-4-4m0 0l4-4m-4 4h18"></path>
+							</svg>
+						</div>
+						<div class="p-2 mb-10">
+							<!-- System Comment -->
+							<div
+								class="flex justify-start flex-col space-y-3 items-start px-2 border-b border-gray-100">
+								<div class="relative mt-1 mb-3 pt-2 flex">
+									<div class="mr-2">
+										<img src="https://avatars.githubusercontent.com/u/68494287?v=4" alt="saman sayyar"
+                                        class="w-8 h-8 rounded-full object-cover">
+                                </div>
+										<div class="ml-2 w-full" x-data="{ replies : false }">
+											<p class="text-gray-600 md:text-sm text-xs w-full">
+												<!-- Username User -->
+												<span class="font-normal text-gray-900">samansayyar</span>
+												<!-- Username User -->
+												You Can see?
+											</p>
+											<div class="flex space-x-4">
+												<div class="time mt-1 text-gray-400 text-xs">
+													<p>2d</p>
+												</div>
+												<button type="button" class="focus:outline-none time mt-1 text-gray-400 text-xs">
+                                            <p>replay</p>
+                                        </button>
+											</div>
+											<button type="button" @click="replies = !replies" class="focus:outline-none  mt-3 flex justify-center items-center">
+                                        <p class="text-xs text-center text-indigo-500 flex space-x-2">
+                                            <span>____ View replies (1)</span> <svg class="w-3 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+                                        </p>
+                                    </button>
+											<div x-show="replies" x-transition
+												class="flex justify-start flex-col space-y-3 items-start px-2 border-b border-gray-100">
+												<div class="relative mt-1 mb-3 pt-2 flex">
+													<div class="mr-2">
+														<img src="https://avatars.githubusercontent.com/u/68494287?v=4" alt="saman sayyar"
+                                                                    class="w-8 h-8 rounded-full object-cover">
+                                                            </div>
+														<div class="ml-2 w-full" x-data="{ replies : true }">
+															<p class="text-gray-600 md:text-sm text-xs w-full">
+																<!-- Username User -->
+																<span class="font-normal text-gray-900">samansayyar</span>
+																<!-- Username User -->
+																You Can see?
+															</p>
+															<div class="flex space-x-4">
+																<div class="time mt-1 text-gray-400 text-xs">
+																	<p>2d</p>
+																</div>
+																<button type="button" class="focus:outline-none time mt-1 text-gray-400 text-xs">
+                                                                        <p>replay</p>
+                                                                    </button>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div
+										class="flex justify-start flex-col space-y-3 items-start px-2 border-b border-gray-100">
+										<div class="relative mt-1 mb-3 pt-2 flex">
+											<div class="mr-2">
+												<img src="https://avatars.githubusercontent.com/u/68494287?v=4" alt="saman sayyar"
+                                        class="w-8 h-8 rounded-full object-cover">
+                                </div>
+												<div class="ml-2 w-full">
+													<p class="text-gray-600 md:text-sm text-xs w-full">
+														<!-- Username User -->
+														<span class="font-normal text-gray-900">samansayyar</span>
+														<!-- Username User -->
+														You Can see?
+													</p>
+													<div class="time mt-1 text-gray-400 text-xs">
+														<p>2d</p>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<!-- System Like and tools Feed -->
+								<div class="flex justify-between items-start p-2 py-">
+									<div class="flex space-x-2 items-center">
+										<button type="button" class="focus:outline-none Like"><svg class="w-8 h-8 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg></button>
+										<button type="button" class="focus:outline-none Comment" @click="comment = !comment"><svg class="w-8 h-8 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg></button>
+										<button type="button" class="focus:outline-none save"><svg class="w-7 h-7 mb-1 ml-1 text-gray-600  z-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg></button>
+									</div>
+									<div class="flex space-x-2 items-center">
+										<button type="button" class="focus:outline-none Like"><svg class="w-8 h-8 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path></svg></button>
+									</div>
+								</div>
+								<div class="p-2 flex flex-col space-y-3">
+									<div class="w-full">
+										<p class="font-bold text-sm text-gray-700">234 likes</p>
+									</div>
+									<div class="w-full">
+										<p class="font-normal text-xs text-gray-500">10 hours ago</p>
+									</div>
+								</div>
+								<!-- End System Like and tools Feed -->
+								<div class="z-50">
+									<form>
+										<div class="flex justify-between border-t items-center w-full"
+											:class="comment ? 'absolute bottom-0' : '' ">
+											<div class="w-full ">
+												<input type="text" name="comment" id="comment" placeholder="Add A Comment..."
+                                    class="w-full text-sm py-4 px-3 rounded-none focus:outline-none">
+                            </div>
+												<div class="w-20">
+													<button class="border-none text-sm px-4 bg-white py-4 text-indigo-600 focus:outline-none">Post</button>
+												</div>
+											</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
