24a8c9fd00ac380f37edfc99978e4a123b19c3c3
서지수 <diduedidue@naver.com>
2024-02-12 11:10:29 +0900

    Feat: 상단바 완성(백버튼+저장)
    Former-commit-id: b90f9edcb44a38cb1f73945362507485efdd5a5d

--- front/src/pages/AddFashion/ImageEdit.jsx
+++ front/src/pages/AddFashion/ImageEdit.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import BackButtonImg from '@/assets/images/back_button_img.png';
+import CropIcon from '@/assets/images/CropIcon.png';
+import EraseIcon from '@/assets/images/EraseIcon.png';
+import MaskingIcon from '@/assets/images/MaskingIcon.png';
+import DoBackIcon from '@/assets/images/DoBackIcon.png';
 
 
 export default function ImageEdit() {
@@ -11,11 +15,28 @@ export default function ImageEdit() {
     };
 
     return (
-        <div>
-            <button onClick={goBack} className="my-1 flex items-start">
-                <img src={BackButtonImg} alt="뒤로 가기" className="size-6 ml-2 mt-1" />
-            </button>
-            <p>이미지에딧</p>
+        <div className="flex flex-col">
+
+            {/* 상단바 부분 */}
+            <div className="bg-gray-200 p-2 flex justify-between items-center">
+                <button onClick={goBack} className="my-1 flex items-start">
+                    <img src={BackButtonImg} alt="뒤로 가기" className="size-6 ml-2 mt-1" />
+                </button>
+                <button className="bg-main text-white font-bold py-2 px-4 rounded">
+                    저장
+                </button>
+            </div>
+
+            {/* 이미지영역 */}
+            <div>
+                <p>이미지에딧</p>
+            </div>
+
+            {/* 하단바 영역 */}
+            <div>
+                <p>버튼들영역</p>
+            </div>
+
         </div>
     );
 }
