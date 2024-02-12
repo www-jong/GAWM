31716e1f4baf289ac79c42bfa2db1517ae46e4a2
서지수 <diduedidue@naver.com>
2024-02-12 11:24:54 +0900

    Feat: 하단바 및 이미지영역 추가
    Former-commit-id: 5354a5bcec531e7c6650aef7e84055a4cd233a94

--- front/src/pages/AddFashion/ImageEdit.jsx
+++ front/src/pages/AddFashion/ImageEdit.jsx
@@ -5,6 +5,8 @@ import CropIcon from '@/assets/images/CropIcon.png';
 import EraseIcon from '@/assets/images/EraseIcon.png';
 import MaskingIcon from '@/assets/images/MaskingIcon.png';
 import DoBackIcon from '@/assets/images/DoBackIcon.png';
+import TestImage from '@/assets/images/test_clothes.png';
+import MaskingEraseIcon from '@/assets/images/MaskingEraseIcon.png';
 
 
 export default function ImageEdit() {
@@ -15,26 +17,49 @@ export default function ImageEdit() {
     };
 
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col min-h-screen">
 
             {/* 상단바 부분 */}
-            <div className="bg-gray-200 p-2 flex justify-between items-center">
+            <div className="bg-gray-100 p-1 flex justify-between items-center">
                 <button onClick={goBack} className="my-1 flex items-start">
                     <img src={BackButtonImg} alt="뒤로 가기" className="size-6 ml-2 mt-1" />
                 </button>
-                <button className="bg-main text-white font-bold py-2 px-4 rounded">
+                <button className="bg-main text-white font-semibold py-1 px-4 rounded">
                     저장
                 </button>
             </div>
 
             {/* 이미지영역 */}
-            <div>
-                <p>이미지에딧</p>
+            <div className="flex-grow flex justify-center items-center bg-black">
+                <img
+                    src={TestImage}
+                    alt="테스트옷이미지"
+                    style={{ maxWidth: '100%', maxHeight: '400px', objectFit: 'contain' }}
+                />
             </div>
 
             {/* 하단바 영역 */}
-            <div>
-                <p>버튼들영역</p>
+            <div className="bg-gray-100 p-2 flex justify-around items-center mt-auto">
+                <button className="flex flex-col items-center">
+                    <img src={CropIcon} alt="크롭" className="w-8 h-8" />
+                    <span className="text-xs mt-1">크롭</span>
+                </button>
+                <button className="flex flex-col items-center">
+                    <img src={EraseIcon} alt="지우개" className="w-8 h-8" />
+                    <span className="text-xs mt-1">지우개</span>
+                </button>
+                <button className="flex flex-col items-center">
+                    <img src={MaskingIcon} alt="마스킹펜" className="w-8 h-8" />
+                    <span className="text-xs mt-1">마스킹</span>
+                </button>
+                <button className="flex flex-col items-center">
+                    <img src={MaskingEraseIcon} alt="마스킹펜" className="w-8 h-8" />
+                    <span className="text-xs mt-1">M지우개</span>
+                </button>
+                <button className="flex flex-col items-center">
+                    <img src={DoBackIcon} alt="리마스킹" className="w-8 h-8" />
+                    <span className="text-xs mt-1">리마스킹</span>
+                </button>
             </div>
 
         </div>
