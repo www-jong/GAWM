607f2026056bfdb5a559597b23671ed7723dd94f
이영욱 <lyuk98@hotmail.com>
2024-02-13 14:20:50 +0900

    Feat: 팔로워 목록 요청 API 생성
    Former-commit-id: e6258766441ad72fd3ba68b9f8ea28e93ebea9a3

--- front/src/apis/user.js
+++ front/src/apis/user.js
@@ -54,6 +54,29 @@ export function updateProfileImge(data) {
 	);
 }
 
+/**
+ * 로그인한 사용자의 팔로워 목록을 받아오는 객체를 생성합니다
+ * 
+ * @param {Object} param0 요청 옵션 (page, size, sortBy, sortDirection)
+ * @returns Promise 객체
+ */
+export function getFollowerList(
+	{
+		page = 0,
+		size = 10,
+		sortBy = "create_at",
+		sortDirection = "asc"
+	}
+) {
+	return axios.get(
+		`${prefix}/follower`,
+		{
+			"params": {
+				page, size, sortBy, sortDirection
+			}
+		}
+	);
+}
 
 /**
  * 로그인한 사용자의 팔로잉 목록을 받아오는 객체를 생성합니다
