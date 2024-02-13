8aa3bf2471ccf8a94917aa2488c56a7e2c6c1349
서지수 <ssafybada@gmail.com>
2024-02-13 15:33:16 +0900

    Feat: 메인페이지 소개 캐러셀 추가
    Former-commit-id: e40aebef5a502b05ec8d5d15b4d1dd10510cc1eb

--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -4,6 +4,15 @@ import HomeLogo from "../../assets/images/HomeLogo.svg";
 import LiveThumbnail from "../../components/LiveThumbnail";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import introduce1 from "@/assets/images/main_introduce1.svg";
+import introduce2 from "@/assets/images/main_introduce2.svg";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import './index.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Autoplay, Pagination } from 'swiper/modules';
 
 /**
  * 홈페이지 component를 생성합니다
@@ -11,9 +20,9 @@ import axios from "axios";
  * @returns 생성된 JSX component
  */
 function Home() {
-    const [liveRooms, setLiveRooms] = useState(undefined);
+	const [liveRooms, setLiveRooms] = useState(undefined);
 
-    useEffect(
+	useEffect(
 		() => {
 			const fetchLiveRooms = async () => {
 				try {
@@ -32,7 +41,7 @@ function Home() {
 		<>
 			{/* 제목 영역 */}
 			<div className="fixed top-0 left-0 right-0 px-2.5 py-1.5 bg-white">
-                <TopBar
+				<TopBar
 					title={
 						<>
 							<img src={HomeLogo} />
@@ -40,11 +49,22 @@ function Home() {
 						</>
 					}
 				/>
-            </div>
+			</div>
 
-            <div className="flex flex-col gap-4 mt-12 mb-24 px-2.5">
+			<div className="flex flex-col gap-4 mt-12 mb-24">
 				{/* 라이브 목록 */}
-				<div className="flex gap-4 items-center scroll-pl-4 pr-4 snap-x overflow-x-auto">
+				<div className="my-2">
+					<Swiper autoplay={{
+						delay: 2500,
+						disableOnInteraction: false,
+					}} pagination={{
+						clickable: true,
+					}} modules={[Autoplay, Pagination]} className="mySwiper">
+						<SwiperSlide><img src={introduce1} alt="소개이미지1" className="flex justify-center items-center" /></SwiperSlide>
+						<SwiperSlide><img src={introduce2} alt="소개이미지2" className="flex justify-center items-center" /></SwiperSlide>
+					</Swiper>
+				</div>
+				<div className="flex gap-4 items-center scroll-pl-4 px-2.5 pr-4 snap-x overflow-x-auto">
 					{
 						liveRooms ? (
 							liveRooms.map(
@@ -64,7 +84,8 @@ function Home() {
 					}
 				</div>
 
-				<div className="">
+
+				<div className="px-2.5">
 					<TodaysLook />
 				</div>
 			</div>
