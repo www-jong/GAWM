5867dea22fa1ae8f144c0599a487bdc7330d8501
서지수 <ssafybada@gmail.com>
2024-02-14 22:10:34 +0900

    Design: 스타일로그 가로 스와이핑
    Former-commit-id: 5e20bb70ea52b5a95df26d8110d767d46e2f5990

--- front/src/components/Modal/StyleLogModal.jsx
+++ front/src/components/Modal/StyleLogModal.jsx
@@ -59,41 +59,47 @@ export default function StyleLog({ date, onClose, stylelogIds }) {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end z-50" onClick={onClose}>
       <div ref={modalRef} className="fixed bottom-0 left-0 right-0 mx-auto p-4 bg-white rounded-t-xl shadow-lg flex flex-col items-center transition duration-300 ease-out translate-y-full" onClick={handleModalContentClick}>
-        <div className="w-8 h-1 rounded-xl mb-3 bg-main"></div>
-        <div className="flex justify-start w-full mt-2">
-          <p className="text-xl font-bold text-main">{date}의 감각</p>
-          {/* {stylelogIds.length > 0 && ( */}
-          {11 > 0 && (
-            <button onClick={handleAddLook} className="bg-orange-500 text-white px-4 py-2 rounded hover:bg-orange-600 transition duration-300 ease-in-out">
-              추가하기
-            </button>
-          )}
-        </div>
-        {/* 스타일로그 이미지를 표시하고, 없으면 기본 이미지를 표시 */}
-        {/* {stylelogImgUrls.length > 0 ? (
+        <div className="my-3 mb-16">
+          <div className="flex justify-center w-full">
+            <p className="text-xl sm:text-xl md:text-2xl font-bold text-main">{date}의 감각</p>
+          </div>
+          <div className="my-2">
+            {/* 스타일로그 이미지를 표시하고, 없으면 기본 이미지를 표시 */}
+            {/* {stylelogImgUrls.length > 0 ? (
           stylelogImgUrls.map((url, index) => (
             <img key={index} className="mt-4 mb-2 w-24 h-24 object-cover" src={import.meta.env.VITE_CLOTHES_BASE_URL+'/'+url} alt={`스타일로그 이미지 ${index}`} />
           ))
         ) : (
           <img className="mt-4 mb-2 w-full" src={StyleLogPlus} onClick={handleAddLook} alt="스타일로그 추가 이미지" />
         )} */}
-        {stylelogImgT.length > 0 ? (
-          <Swiper
-            effect={'cards'}
-            grabCursor={true}
-            modules={[EffectCards]}
-            className="mySwiper"
-          >
-            {stylelogImgT.map((url, index) => (
-              <SwiperSlide key={index}>
-                <img key={index} src={stylelogImgT[index]} alt={`스타일로그 이미지 ${index}`} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        ) : (
-          <img className="mt-4 mb-2 w-full" src={StyleLogPlus} onClick={handleAddLook} alt="스타일로그 추가 이미지" />
-        )}
+            {stylelogImgT.length > 0 ? (
+              <Swiper
+                effect={'cards'}
+                grabCursor={true}
+                modules={[EffectCards]}
+                className="mySwiper"
+              >
+                {stylelogImgT.map((url, index) => (
+                  <SwiperSlide key={index}>
+                    <img key={index} src={stylelogImgT[index]} alt={`스타일로그 이미지 ${index}`} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            ) : (
+              <img className="mt-4 mb-2 w-full" src={StyleLogPlus} onClick={handleAddLook} alt="스타일로그 추가 이미지" />
+            )}
+          </div>
+
+        </div>
       </div>
+      {/* {stylelogIds.length > 0 && ( */}
+      {11 > 0 && (
+        <div className="fixed inset-x-0 bottom-0 bg-white shadow-lg">
+          <button onClick={handleAddLook} className="w-full text-lg bg-main text-white py-3 transition duration-300 ease-in-out flex-grow">
+            추가하기
+          </button>
+        </div>
+      )}
     </div>
   );
 }
